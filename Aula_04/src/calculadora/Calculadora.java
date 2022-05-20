dd16d9514acfa4f3cc4f7c5e3f8cabc597b75697
Matheus <matheusvictorgdr15@gmail.com>
2022-05-20 11:31:53 -0300

    Atualização da aula 04

--- Aula_04/src/calculadora/Calculadora.java
+++ Aula_04/src/calculadora/Calculadora.java
@@ -2,31 +2,38 @@ package calculadora;
 
 public class Calculadora {
 	
-	private int valor1;
-	private int valor2;
+	private double valor1;
+	private double valor2;
 	
-	public int getValor1() {
+	public double getValor1() {
 		return valor1;
 	}
 	public void setValor1(int valor1) {
 		this.valor1 = valor1;
 	}
-	public int getValor2() {
+	public double getValor2() {
 		return valor2;
 	}
 	public void setValor2(int valor2) {
 		this.valor2 = valor2;
 	}
 	
-	public int somar() {
+	public double somar(double a, double b) {
+		this.valor1 = a;
+		this.valor2 = b;
+		
 		return valor1 + valor2;
 	}
 	
-	public int subtrair() {
+	public double subtrair(double a, double b) {
+		this.valor1 = a;
+		this.valor2 = b;
 		return valor1 - valor2;
 	}
 	
-	public int multiplicar() {
+	public double multiplicar(double a, double b) {
+		this.valor1 = a;
+		this.valor2 = b;
 		return valor1 * valor2;
 	}
 	
--- Aula_04/src/calculadora/Conta.java
+++ Aula_04/src/calculadora/Conta.java
@@ -5,15 +5,19 @@ public class Conta {
 	public static void main(String[] args) {
 		
 		Calculadora cal = new Calculadora();
+	
+			/*cal.setValor1(12);
+			cal.setValor2(6);
+			
+			System.out.println(cal.somar(4,5));
+			System.out.println(cal.subtrair());
+			System.out.println(cal.multiplicar());
+			System.out.println(cal.dividir());*/
 		
-		cal.setValor1(12);
-		cal.setValor2(6);
+		System.out.println(cal.somar(5, 10));
+		System.out.println(cal.subtrair(4, 8));
+			
+		}
 		
-		System.out.println(cal.somar());
-		System.out.println(cal.subtrair());
-		System.out.println(cal.multiplicar());
-		System.out.println(cal.dividir());
-
 	}
 
-}
