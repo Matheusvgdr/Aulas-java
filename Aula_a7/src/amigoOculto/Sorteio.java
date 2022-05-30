5aa3af2ee8fe0e5ce5286310733ef9a4abf7a50c
Técnico Informática 2021.1 <51127512021.1@senacrjedu.local>
2022-05-30 08:37:39 -0300

    Reparo na aula 07

--- Aula_a7/src/amigoOculto/Sorteio.java
+++ Aula_a7/src/amigoOculto/Sorteio.java
@@ -3,36 +3,49 @@ package amigoOculto;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Sorteio {
 
 	public static void main(String[] args) {
 		
-		Random index = new Random();		
+		//Instances
+		Random index = new Random();
 		
+		//Scanner int
+		Scanner numPessoas = new Scanner(System.in);
+		
+		//Scanner String
+		Scanner resposta = new Scanner(System.in);
+		
+		//Variables
+		int valor;
+		String respostaSorteio;
+		
+		//--------Program-----------
+		
+		System.out.println("Quantas pessoas deseja sortear?");
+		valor = numPessoas.nextInt();
+		
+		//Array
 		List<String> nomes = new ArrayList<String>();
 		
-		nomes.add("Matheus");
-		nomes.add("Nicolas");
-		nomes.add("Marcos");
-		nomes.add("Arian");
-		nomes.add("Felipe");
-		nomes.add("Beatriz");
+		for(int i = 0; i < valor; i++) {
+			System.out.println("O nome da pessoa: ");
+			
+			nomes.add(resposta.next());
+		}
 		
+		System.out.println("Deseja realizar o sorteio? S/N");
+		respostaSorteio = resposta.next();
 		
-		 System.out.println("O nome é: " + nomes.get(index.nextInt(nomes.size())));
-		 
-		 
-		 
-		/*
-			for(int i = 0; i <1; i++) {
-				
-				valor = index.nextInt(nomes.size());
-				
-				System.out.println(nomes.get(valor)); 
-				
-			}
-		 */
+		if(respostaSorteio.equals("S")) {
+			System.out.println("A pessoa sorteada foi: " + nomes.get(index.nextInt(nomes.size())));
+		}else if(respostaSorteio.equals("N")) {
+			System.out.println("O sorteio foi finalizado");
+		}else {
+			System.out.println("Resposta inválida");
+		}
 	}
 
 }
